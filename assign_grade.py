68dac08f37806149ec307ae1ea777a0f631ec748
Arjun Ashok <arjun3.ashok@gmail.com>
2023-03-02 00:04:25 -0800

    File setup for risk and grade

---
+++ assign_grade.py
@@ -0,0 +1,4 @@
+"""
+    This algorithm takes the results of the multi-regressive time series analysis to assign a grade
+    to countries based on their efforts in protecting the natural landscapes within their borders.
+"""
---
+++ risk_analysis.py
@@ -0,0 +1,4 @@
+"""
+    This algorithm takes the results of the multi-regressive time series analysis to calculate future risk of
+    a specific ecosystem being damaged beyond the point of recovery.
+"""
