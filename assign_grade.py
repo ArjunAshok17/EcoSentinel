920f85e13f8403b15ce8fc902920b4e1edf37a9f
Arjun Ashok <arjun3.ashok@gmail.com>
2023-03-28 13:23:53 -0700

    Created simple data labeler (note, unreliable/unrobust, should not be main source)

--- assign_grade.py
+++
@@ -1,4 +0,0 @@
-"""
-    This algorithm takes the results of the multi-regressive time series analysis to assign a grade
-    to countries based on their efforts in protecting the natural landscapes within their borders.
-"""
